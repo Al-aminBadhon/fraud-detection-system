bc4796529c639882387087a3c9627e37cabaae65
B M Al-amin Badhon <alamin.badhonn@gmail.com>
2025-09-28 16:07:44 -0230

    Initial commit

---
+++ src/test/java/com/dev/FraudDetection/FraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.FraudDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
